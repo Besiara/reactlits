2fd03cad7ea0eb85acf21bf961f44ccec80ea35b
Bessonov Oleksandr <alex.bessonov85@gmail.com>
2016-08-27 12:30:51 +0300

    first commit

---
+++ chat-app/imports/ui/Chat.jsx
@@ -0,0 +1,45 @@
+import Moment from 'moment';
+
+import React, { Component, PropTypes } from 'react';
+
+import { Card, CardHeader, CardActions } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
+export default class Chat extends Component {
+  getTime() {
+    if (!this.props.chat.lastMessage.timestamp) return;
+
+    return Moment(this.props.chat.lastMessage.timestamp).calendar(null, {
+      lastDay : '[Yesterday]',
+      sameDay : 'LT',
+      lastWeek : 'dddd',
+      sameElse : 'DD/MM/YY'
+    });
+  }
+
+  render() {
+    const cardStyles = {
+      marginBottom: 10,
+    };
+    return (
+      <Card style={cardStyles}>
+        <CardHeader
+          title={this.props.chat.name}
+          subtitle={<p>{this.props.chat.lastMessage.text} <b>{this.getTime()}</b></p>}
+          avatar={this.props.chat.picture}
+        />
+        <CardActions>
+          <FlatButton
+          label="Delete Chat"
+          onClick={() => this.props.deleteChat(this.props.chat)}
+          />
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+Chat.propTypes = {
+  chat: PropTypes.object.isRequired,
+  deleteChat: PropTypes.func.isRequired
+};
